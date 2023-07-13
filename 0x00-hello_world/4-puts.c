2f61176a93930f997407e9f5d21005f48fd69dc2
nwidagkingsley <nkkingsley01@gmail.com>
2023-07-13 14:46:11 -0700

    writing my first code on c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - this is a main function
+ *
+ * Return: alway 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle);
+	return (0)
+}
