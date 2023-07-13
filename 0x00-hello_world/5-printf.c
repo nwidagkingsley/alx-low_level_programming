661f57d8deed66de93485c0c1a6fca0bbee1dce8
nwidagkingsley <nkkingsley01@gmail.com>
2023-07-13 14:59:32 -0700

    it began makin sense

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function of the code
+ *
+ * Return: this will return when it is succesful
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
