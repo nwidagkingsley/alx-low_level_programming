af5f362872db60911803d35ac2b34c1235e1fbd0
nwidagkingsley <nkkingsley01@gmail.com>
2023-07-15 22:36:44 -0700

    fhe

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,10 +12,7 @@ int main(void)
 	{
 		for (j = i + 1; j <= 9; j++)
 		{
-			putchar((i / 10) + '0');
 			putchar((i % 10) + '0');
-			putchar(' ');
-			putchar((j / 10) + '0');
 			putchar((j % 10) + '0');
 
 			if (i != 8 || j != 9)
