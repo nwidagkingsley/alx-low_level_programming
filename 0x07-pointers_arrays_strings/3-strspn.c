4e75895dd9358f0c404d4f609d7a62f53d67062e
nwidagkingsley <nkkingsley01@gmail.com>
2023-07-31 18:37:28 -0700

    great

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strspan - spans through numbers
+ * @s: string tto search from
+ * @accept: string to search
+ * Return: any nmber
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; s[j] != '\0'; j++)
+		{
+			if (accept[j] == '\0')
+				return (i);
+		}
+	}
+	return (0);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -6,4 +6,6 @@ int _putchar (char c);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 #include <stdio.h>
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
 #endif
