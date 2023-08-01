38028055cf2551134e58515b7f73b745c68172cd
nwidagkingsley <nkkingsley01@gmail.com>
2023-07-31 17:58:38 -0700

    this coppies strig

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _2-strchr.c - string copyng
+ * @s: this is the strig we are going to search for
+ * @c: this is the cahracter we are going to search for in s
+ * Return: this wil return a value
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s ==  c)
+			return (s);
+		s++;
+	}
+	if (*s == c)
+		return (s);
+	return (NULL);
+}
